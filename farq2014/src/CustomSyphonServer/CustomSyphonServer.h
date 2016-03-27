d77fe17d141c0897e2c66b87bb7aae58a845ff37
Nicolas Peri <nperi89@gmail.com>
2016-03-27 01:57:24 -0300

    Syphon publisher added

--- farq2014/src/CustomSyphonServer/CustomSyphonServer.h
+++ farq2014/src/CustomSyphonServer/CustomSyphonServer.h
@@ -14,20 +14,38 @@
 #include "ofxSyphon.h"
 
 
-class CustomSyphonServer {
+class CustomSyphonServer : public ImageOutput {
     
 public:
     
-    CustomSyphonServer(string exportName_, ImageOutput* feeder_);
+    CustomSyphonServer(string exportName_ = "new export name", ImageOutput* feeder_ = NULL, string name_ = "Syphon Server", int id_ = -1);
+    ~CustomSyphonServer();
+    void update();
     void setup();
-    void publishTexture();
-    
     bool saveSettings(ofxXmlSettings &XML);
+    bool loadSettings(ofxXmlSettings &XML, int nTag_, int nodesCount_ = 0);
+    bool saveSettingsToSnippet(ofxXmlSettings &XML, map<int,int> newIdsMap) {};
+
+    string getExportName();
+    void   setExportName(string newExportName);
     
-    string exportName;
+    ofImage*    getImage();
+    ofTexture*  getTexture();
+    
+    void updateParameter(Param* inputParam) {};
+    float getMidiMin(string param_) {};
+    float getMidiMax(string param_) {};
+    
+private:
     ImageOutput* feeder;
     ofxSyphonServer server;
     
+    void inputAdded(ImageOutput* in_);
+    void inputRemoved(int id_);
+    
+    string exportName;
+    ofxTextInput exportNameText;
+    ofParameter<string> exportNameParam;
 };
 
 
